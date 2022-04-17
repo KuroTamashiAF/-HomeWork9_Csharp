f8d81095c01fccae31ef7e7d86da6cc777a41d6e
Alex <chugun777akk@mail.ru>
2022-04-17 13:35:21 +0700

    task4 is done

--- HW9Task4/Program.cs
+++ HW9Task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем
+int Fibonachi(int n)
+{
+    if (n == 1 || n == 2)
+        return 1;
+
+    else
+        return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
+Console.Write("Enter number N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Fibonachi(i) + " ");
+}
+
