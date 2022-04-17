599b83566db238cec1115cb7fcc3d0963c137ca4
Alex <chugun777akk@mail.ru>
2022-04-17 11:55:20 +0700

    task1,2 is done

---
+++ HW9Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму цифр числа
+int SumDigitsNumber(int number)
+{
+    if (number != 0)
+    {
+        return number%10 + SumDigitsNumber(number/10);
+    }
+    else 
+        return number;
+
+}
+Console.Write("Enter the number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int sum = SumDigitsNumber(Number);
+Console.Write(sum);
+
+
---
+++ HW9Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу вычисления функции Аккермана
+int FunctionOfAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return FunctionOfAkkerman(m - 1, n = 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return FunctionOfAkkerman(m - 1, FunctionOfAkkerman(m, n - 1));
+    }
+    return -1;
+}
+Console.Write("Enter number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(FunctionOfAkkerman(m, n));
+
