26b75bede5235f15a1a02f2c7290b9c6f7bd90ce
Alex <chugun777akk@mail.ru>
2022-04-17 12:54:03 +0700

    task3 is done

--- HW9Task3/Program.cs
+++ HW9Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу возведения числа А в целую стень B
+int DegreeNumberA(int a, int degree)
+{
+    if (degree >= 1)
+    {
+        return a * DegreeNumberA(a, degree - 1);
+    }
+    return 1;
+}
+Console.Write("Enter the number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the degree: ");
+int Degree = Convert.ToInt32(Console.ReadLine());
+Console.Write(DegreeNumberA(A, Degree));
+
+
